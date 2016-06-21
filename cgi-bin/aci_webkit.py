656140287f5a42d419af39d7cde3e7d2ded5397a
hyjang <hyjang@10.139.4.214>
2016-06-21 10:41:23 +0900

    commit

--- cgi-bin/aci_webkit.py
+++ cgi-bin/aci_webkit.py
@@ -512,7 +512,7 @@ def show_dashboard(rest):
             <div class="circleStatsItemBox">
                 <div class="header">Tenants</div>
                 <div class="circleStat">
-                    <input type="text" value="@fvTenant_pct" class="whiteCircle" />
+                    <input type="text" value="@fvTenant_cnt" class="whiteCircle" />
                 </div>
                 <span class="countpanel">@fvTenant_cnt</span>
                 <div class="footer">
